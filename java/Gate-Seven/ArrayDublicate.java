b16f2529e75b1edf8b4dda389867eb6ce0741e5f
Value Taire <talk2valuetaire@gmail.com>
2026-03-26 10:51:50 +0100

    phasegate

---
+++ java/Gate-Seven/ArrayDublicate.java
@@ -0,0 +1,46 @@
+public class ArrayDublicate{
+
+int[] numbers = new int [5];
+
+	static void  checkThroughTheArray();
+			numbers = {9, 2, 3, 2, 1};
+	  	System.out.println("Numbers Without Duplicates:");
+	  	
+		for (int count = 0; count < numbers.length; count++){
+				boolean checkIfIsDublicate = false;
+				
+				for (int index = 0; index < count; index++){
+					if (number[count] == number[index]){
+						checkIfIsDublicate = true;
+						
+						
+						if (!checkIfIsDublicate){
+						System.out.println(numbers[count]);
+						
+						}
+					
+					}
+				
+				
+				}
+				
+				}
+				public static void main(String...args){
+					checkThroughTheArray();
+				
+				
+				}
+				
+				
+				
+				
+				
+				
+				
+				
+				
+	
+
+
+
+}
